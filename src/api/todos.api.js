63d947e2dcb494c7219e1495c8803c5020e34e91
Andrés Amaya Díaz <andres.amaya.diaz@gmail.com>
2018-08-26 18:51:45 -0500

    API HOST

--- src/api/todos.api.js
+++ src/api/todos.api.js
@@ -2,24 +2,24 @@ import axios from "axios";
 
 export default {
     getTodos: function() {
-      return axios.get("http://127.0.0.1:3001/api/todos");
+      return axios.get(process.env.API_URL + "/api/todos");
     },
     getTodo: function(id) {
-      return axios.get("http://127.0.0.1:3001/api/todos/" + id);
+      return axios.get(process.env.API_URL + "/api/todos/" + id);
     },
     getTodosCompleted: function() {
-        return axios.get("http://127.0.0.1:3001/api/todos/completed");
+        return axios.get(process.env.API_URL + "/api/todos/completed");
     },
     getTodosUncompleted: function() {
-        return axios.get("http://127.0.0.1:3001/api/todos/uncompleted");
+        return axios.get(process.env.API_URL + "/api/todos/uncompleted");
     },
     postTodos: function(todo) {
-        return axios.post("http://127.0.0.1:3001/api/todos", todo);
+        return axios.post(process.env.API_URL + "/api/todos", todo);
     },
     putTodos: function(todo) {
-        return axios.put("http://127.0.0.1:3001/api/todos/"+todo.id, todo);
+        return axios.put(process.env.API_URL + "/api/todos/"+todo.id, todo);
     },
     deleteTodos: function(id) {
-        return axios.delete("http://127.0.0.1:3001/api/todos/"+id);
+        return axios.delete(process.env.API_URL + "/api/todos/"+id);
     }
   };
