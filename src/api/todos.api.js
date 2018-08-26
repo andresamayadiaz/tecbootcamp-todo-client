f0d927255fd55b7f1e1b00f63862b838b6236c84
Andrés Amaya Díaz <andres.amaya.diaz@gmail.com>
2018-08-26 18:28:12 -0500

    API Changes

--- src/api/todos.api.js
+++ src/api/todos.api.js
@@ -2,24 +2,24 @@ import axios from "axios";
 
 export default {
     getTodos: function() {
-      return axios.get("/api/todos");
+      return axios.get("http://127.0.0.1:3001/api/todos");
     },
     getTodo: function(id) {
-      return axios.get("/api/todos/" + id);
+      return axios.get("http://127.0.0.1:3001/api/todos/" + id);
     },
     getTodosCompleted: function() {
-        return axios.get("/api/todos/completed");
+        return axios.get("http://127.0.0.1:3001/api/todos/completed");
     },
     getTodosUncompleted: function() {
-        return axios.get("/api/todos/uncompleted");
+        return axios.get("http://127.0.0.1:3001/api/todos/uncompleted");
     },
     postTodos: function(todo) {
-        return axios.post("/api/todos", todo);
+        return axios.post("http://127.0.0.1:3001/api/todos", todo);
     },
     putTodos: function(todo) {
-        return axios.put("/api/todos/"+todo.id, todo);
+        return axios.put("http://127.0.0.1:3001/api/todos/"+todo.id, todo);
     },
     deleteTodos: function(id) {
-        return axios.delete("/api/todos/"+id);
+        return axios.delete("http://127.0.0.1:3001/api/todos/"+id);
     }
   };
